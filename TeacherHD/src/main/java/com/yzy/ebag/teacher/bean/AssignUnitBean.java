63c344fdc5c755cac6336cbdc8eae55859360fc1
Jeremy <liyimin_bury@126.com>
2018-02-23 18:10:54 +0800

    试卷相关

--- TeacherHD/src/main/java/com/yzy/ebag/teacher/bean/AssignUnitBean.java
+++ TeacherHD/src/main/java/com/yzy/ebag/teacher/bean/AssignUnitBean.java
@@ -37,8 +37,8 @@ public class AssignUnitBean implements
     }
 
     public int getId() {
-                return id;
-            }
+        return Integer.parseInt(unitCode);
+    }
 
     public void setId(int id) {
         this.id = id;
