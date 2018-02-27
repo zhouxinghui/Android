1bab9641e28c6d1e1ccb4cf63aef6ac32afbd93a
wy <1>
2018-02-27 15:44:34 +0800

    第三方登录--新浪失败。

---
+++ TeacherHD/src/main/java/com/yzy/ebag/teacher/wxapi/WBShareActivity.java
@@ -0,0 +1,10 @@
+package com.yzy.ebag.teacher.wxapi;
+
+import com.umeng.socialize.media.WBShareCallBackActivity;
+
+/**
+ * Created by ysb on 2018/2/27.
+ */
+
+public class WBShareActivity extends WBShareCallBackActivity {
+}
