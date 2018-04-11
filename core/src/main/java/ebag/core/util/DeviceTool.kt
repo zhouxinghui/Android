ad6aafd12e7a0ae87018e5ee71f5b38afa9ca784
fansan <fansan@live.com>
2018-04-11 10:44:55 +0800

    添加获取imei工具类

---
+++ core/src/main/java/ebag/core/util/DeviceTool.kt
@@ -0,0 +1,26 @@
+package ebag.core.util
+
+import android.Manifest.permission.READ_PHONE_STATE
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.support.annotation.RequiresPermission
+import android.telephony.TelephonyManager
+
+object DeviceTool {
+
+    @SuppressLint("MissingPermission")
+    @RequiresPermission(READ_PHONE_STATE)
+    fun getIMEI(context: Context): String {
+
+        val telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            telephonyManager.imei
+        } else {
+            telephonyManager.deviceId
+        }
+
+    }
+
+}
