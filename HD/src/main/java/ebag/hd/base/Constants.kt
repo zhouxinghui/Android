07751401575ab5c1a6d5b07721ee4d98ac6aecf9
fansan <fansan@live.com>
2018-04-12 14:15:47 +0800

    我的班级点击老师showdialog

--- HD/src/main/java/ebag/hd/base/Constants.kt
+++ HD/src/main/java/ebag/hd/base/Constants.kt
@@ -65,10 +65,10 @@ object Constants {
     const val THIRD_PARTY_UNION_ID = "thirdPartyUnionid"
 
 //    const val BASE_URL = "http://www.yun-bag.com/ebag-portal/"
-//    const val BASE_URL = "http://192.168.1.141:9001/"
+    const val BASE_URL = "http://192.168.1.141:9001/"
 //    const val BASE_URL = "http://192.168.1.155:9001/"
 //    const val BASE_URL = "http://192.168.1.135:9001/"
 //    const val BASE_URL = "http://192.168.1.105:9001/"
 //    const val BASE_URL = "http://192.168.1.110:9001/"
-    const val BASE_URL = "http://localhost:9001/"
+//    const val BASE_URL = "http://localhost:9001/"
 }
